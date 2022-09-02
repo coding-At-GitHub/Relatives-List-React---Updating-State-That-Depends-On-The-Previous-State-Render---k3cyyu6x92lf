e7f95a6b84a4e42da081475dee1c13dc6d594fad
Suresh Dukiya <rjsh1503@gmail.com>
2022-09-02 23:41:54 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+        let friends = ["Raju", "Mohan", "Ramu"];
         return(
             <div id="main">
                {/* Do not remove the main div */}
+                <ol key="relativeList">
+                  {friends.map((friend, index) => (
+                    <li key={"relativeListItem" + (index + 1)} />
+                  ))}
+            </ol>
             </div>
         )
     }
